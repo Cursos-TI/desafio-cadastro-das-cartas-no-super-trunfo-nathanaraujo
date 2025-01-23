94f97d2ea50df31d652e39b02ff4412af5f04599
nathanaraujo <nathanaraujo@gmail.com>
2025-01-23 19:25:36 +0000

    add primeiro printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,6 +9,7 @@
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    printf ("Desafio Super Trunfo - Países \n\n")
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
