1598fe891b636193e14ee4551dc5655b0f2b54d4
nathanaraujo <nathanaraujo@gmail.com>
2025-01-23 20:36:29 +0000

    add variaveis de cadastro do jogo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,12 +15,29 @@ int main() {
     int numCidade;
     int populacao;
     float area;
-    float pib;
+    double pib;
     int pontosTur;
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    printf("Qual é o país? (A-H):\n");
+    scanf("%c%", pais);
+
+    printf("Qual é o codigo da cidade? (01-04):\n");
+    scanf("%d", numCidade);
+
+    printf("Qual é a população da cidade?:\n");
+    scanf("%d", populacao);
+
+    printf("Qual é a área da cidade? (km²):\n");
+    scanf("%.1f", area);
+
+    printf("Qual é o PIB da cidade (em bilhões de reais):\n");
+    scanf("%.2f", pib);
+
+    print("Quantos pontos turísticos têm na cidade?:\n");
+    scanf("%d", pontosTur);
     
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
