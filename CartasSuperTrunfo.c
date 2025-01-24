f62cd1dbc1dc8eee27e87d22c4eaeaed5c53a4cb
nathanaraujo <nathanaraujo@gmail.com>
2025-01-24 17:57:07 +0000

    arquivo final do jogo funcionando

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// Início do código
 
 int main() {
     printf ("Desafio Super Trunfo - Países \n");
 
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    // Declaração de variáveis do jogo
+
     char estado[32];
     char nomeCidade[50];
     int numCidade;
@@ -19,9 +16,9 @@ int main() {
     double pib;
     int pontosTur;
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    // Cadastro das Cartas
+    // Solicita ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+
 
     printf("Qual é o nome da cidade?\n");
     scanf("%[^\n]", &nomeCidade);
@@ -49,14 +46,12 @@ int main() {
     scanf("%d", &pontosTur);
 
     // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de form
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // Exibe confirmação de cadastro e os valores inseridos para cada atributo da cidade, um por linha.
 
     printf("\n\n\n##### CARTA CADASTRADA #####\n");
     
     printf("____________________________\n\n");
     printf("%s • %s0%d\n\n", nomeCidade, estado, numCidade);
-    //printf("\nCÓDIGO DA CIDADE - %s0%d\n", estado, numCidade);
     printf("POPULAÇÃO - %d pessoas\n", populacao);
     printf("ÁREA - %.2f km²\n", area);
     printf("PIB - %.2lf milhões de reais\n", pib);
